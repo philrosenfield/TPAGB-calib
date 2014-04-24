80f6a7b3b633416fb92525b6466f0baa36ada4bf
Phil Rosenfield <philrosenfield@gmail.com>
2014-04-24 15:28:51 +0200

    makinging calibration pkg

--- utils.py
+++
@@ -1,9 +0,0 @@
-def combine_list_of_dictionaries(dlist):
-    result_dict = {}
-    for dic in dlist:
-        for key in dic.keys():
-            if not key in result_dict.keys():
-                result_dict[key] = []
-            result_dict[key].append(dic[key])
-
-    return result_dict
