a75144f41109d88a88b696c0021e9e41508901cc
Phil Rosenfield <philrosenfield@gmail.com>
2015-01-19 17:13:45 -0800

    minor edits to lf plot

--- tpagb_calibration/plotting/plotting.py
+++ tpagb_calibration/plotting/plotting.py
@@ -363,8 +363,8 @@ def compare_to_gal(matchphot=None, lf_file=None, limit=None, draw_lines=True,
     if xlim is not None:
         ax.set_xlim(xlim)
     else:
-        ax.set_xlim(np.min(np.concantenate((mag2, mag2s))),
-                    np.max(np.concantenate((mag2, mag2s))))
+        ax.set_xlim(np.min(np.concatenate((mag2, mag2s))),
+                    np.max(np.concatenate((mag2, mag2s))))
 
     if draw_lines:
         if mag_bright is not None:
