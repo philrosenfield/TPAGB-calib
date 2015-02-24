1925880a1d04bd452ea4498aa88ebcf794c05b3f
Phil Rosenfield <philrosenfield@gmail.com>
2015-02-24 11:24:05 -0800

    messing with varysfh bashscript

--- firstrun_vary_sfh.sh
+++ firstrun_vary_sfh.sh
@@ -10,21 +10,21 @@ for galaxy in eso540-030 scl-de1 ugc-04459 ugc-4305-2 ugc-5139 ugc8508 ddo78 kdg
 do
     echo $galaxy
     #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -d $galaxy
-    echo "$CMD $LOC/$galaxy/$galaxy.vsfhinp > $loc/$galaxy/$galaxy.vsfhout"
+    echo "$CMD $LOC/$galaxy/$galaxy.vsfhinp > $LOC/$galaxy/$galaxy.vsfhout"
     #> $galaxy_vsfh.sh
     #python -m tpagb_calibration.plotting.plotting /home/rosenfield/research/TP-AGBcalib/SNAP/varysfh/$galaxy/$galaxy.plotinp
 done
 # more than one filter in SNAP/tables/snap_galaxies.dat
 echo "ic2574-sgs"
 #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -df 555 ic2574-sgs
-echo "$CMD $LOC/ic2574-sgs/ic2574-sgs_555.vsfhinp > $loc/ic2574-sgs/ic2574-sgs_555.vsfhout"
+echo "$CMD $LOC/ic2574-sgs/ic2574-sgs_555.vsfhinp > $LOC/ic2574-sgs/ic2574-sgs_555.vsfhout"
 #> ic2574-sgs_555_vsf.sh
 
 for galaxy in ugca292 ngc300-wide1
 do
     echo $galaxy
     #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -df 475 $galaxy
-    echo "$CMD $LOC/$galaxy/$galaxy_475.vsfhinp > $loc/$galaxy/$galaxy_475.vsfhout &"
+    echo "$CMD $LOC/$galaxy/$galaxy\_475.vsfhinp > $LOC/$galaxy/$galaxy\_475.vsfhout &"
     #> $galaxy_475_vsfh.sh
 done
 
@@ -32,7 +32,7 @@ for galaxy in ugca292 ngc300-wide1 ddo82 ngc4163
 do
     echo $galaxy
     #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -df 606 $galaxy
-    echo "$CMD $LOC/$galaxy/$galaxy_606.vsfhinp > $loc/$galaxy/$galaxy_606.vsfhout &"
+    echo "$CMD $LOC/$galaxy/$galaxy\_606.vsfhinp > $LOC/$galaxy/$galaxy\_606.vsfhout &"
     #> $galaxy_606_vsfh.sh
 done
 
