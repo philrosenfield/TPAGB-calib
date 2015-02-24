ecd69790ac85fda9c917be01899b2531176ad586
Phil Rosenfield <philrosenfield@gmail.com>
2015-02-24 11:29:39 -0800

    messing with varysfh bashscript

--- firstrun_vary_sfh.sh
+++ firstrun_vary_sfh.sh
@@ -11,26 +11,23 @@ for galaxy in eso540-030 scl-de1 ugc-04459 ugc-4305-2 ugc-5139 ugc8508 ddo78 kdg
 do
     echo $galaxy
     #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -d $galaxy
-    echo "$CMD $LOC/$galaxy/$galaxy.vsfhinp > $LOC/$galaxy/$galaxy.vsfhout"
-    echo "> $galaxy$EXT"
+    echo "$CMD $LOC/$galaxy/$galaxy.vsfhinp > $LOC/$galaxy/$galaxy.vsfhout" > $galaxy$EXT
     #python -m tpagb_calibration.plotting.plotting /home/rosenfield/research/TP-AGBcalib/SNAP/varysfh/$galaxy/$galaxy.plotinp
 done
+
 # more than one filter in SNAP/tables/snap_galaxies.dat
 FILT="_555"
 GALAXY="ic2574-sgs"
-
 echo $GALAXY
 #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -df 555 ic2574-sgs
-echo "$CMD $LOC/$GALAXY/$GALAXY$FILT.vsfhinp > $LOC/$GALAXY/$GALAXY$FILT.vsfhout"
-echo "> $GALAXY$FILT$EXT"
+echo "$CMD $LOC/$GALAXY/$GALAXY$FILT.vsfhinp > $LOC/$GALAXY/$GALAXY$FILT.vsfhout" > $GALAXY$FILT$EXT
 
 FILT="_475"
 for galaxy in ugca292 ngc300-wide1
 do
     echo $galaxy
     #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -df 475 $galaxy
-    echo "$CMD $LOC/$galaxy/$galaxy$FILT.vsfhinp > $LOC/$galaxy/$galaxy$FILT.vsfhout &"
-    echo "> $galaxy$FILT$EXT"
+    echo "$CMD $LOC/$galaxy/$galaxy$FILT.vsfhinp > $LOC/$galaxy/$galaxy$FILT.vsfhout &" > $galaxy$FILT$EXT
 done
 
 FILT="_606"
@@ -38,8 +35,7 @@ for galaxy in ugca292 ngc300-wide1 ddo82 ngc4163
 do
     echo $galaxy
     #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -df 606 $galaxy
-    echo "$CMD $LOC/$galaxy/$galaxy$FILT.vsfhinp > $LOC/$galaxy/$galaxy$FILT.vsfhout &"
-    echo "> $galaxy$FILT$EXT"
+    echo "$CMD $LOC/$galaxy/$galaxy$FILT.vsfhinp > $LOC/$galaxy/$galaxy$FILT.vsfhout &" > $galaxy$FILT$EXT"
 done
 
 #ipcluster stop
