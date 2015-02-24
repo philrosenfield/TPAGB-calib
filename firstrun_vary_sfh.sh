c741e17db1b9e1e37552f55f968515aed3d1c4b5
Phil Rosenfield <philrosenfield@gmail.com>
2015-02-24 11:28:23 -0800

    messing with varysfh bashscript

--- firstrun_vary_sfh.sh
+++ firstrun_vary_sfh.sh
@@ -5,22 +5,24 @@
 #ipcluster start -n=$1 &
 CMD="nice -n 19 python -m tpagb_calibration.sfhs.vary_sfh"
 LOC="/home/rosenfield/research/TP-AGBcalib/SNAP/varysfh"
+EXT="_vsfh.sh"
 # only one filter in SNAP/tables/snap_galaxies.dat
 for galaxy in eso540-030 scl-de1 ugc-04459 ugc-4305-2 ugc-5139 ugc8508 ddo78 kdg73 kkh37 ngc3741 hs117 ngc2403-deep
 do
     echo $galaxy
     #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -d $galaxy
     echo "$CMD $LOC/$galaxy/$galaxy.vsfhinp > $LOC/$galaxy/$galaxy.vsfhout"
-    #> $galaxy_vsfh.sh
+    echo "> $galaxy$EXT"
     #python -m tpagb_calibration.plotting.plotting /home/rosenfield/research/TP-AGBcalib/SNAP/varysfh/$galaxy/$galaxy.plotinp
 done
 # more than one filter in SNAP/tables/snap_galaxies.dat
 FILT="_555"
 GALAXY="ic2574-sgs"
+
 echo $GALAXY
 #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -df 555 ic2574-sgs
 echo "$CMD $LOC/$GALAXY/$GALAXY$FILT.vsfhinp > $LOC/$GALAXY/$GALAXY$FILT.vsfhout"
-echo "> $GALAXY$FILT_vsf.sh"
+echo "> $GALAXY$FILT$EXT"
 
 FILT="_475"
 for galaxy in ugca292 ngc300-wide1
@@ -28,7 +30,7 @@ do
     echo $galaxy
     #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -df 475 $galaxy
     echo "$CMD $LOC/$galaxy/$galaxy$FILT.vsfhinp > $LOC/$galaxy/$galaxy$FILT.vsfhout &"
-    echo "> $galaxy$FILT_vsfh.sh"
+    echo "> $galaxy$FILT$EXT"
 done
 
 FILT="_606"
@@ -37,7 +39,7 @@ do
     echo $galaxy
     #python /home/rosenfield/research/TP-AGBcalib/code/TPAGB-calib/tpagb_calibration/prepare_data.py -n $1 -df 606 $galaxy
     echo "$CMD $LOC/$galaxy/$galaxy$FILT.vsfhinp > $LOC/$galaxy/$galaxy$FILT.vsfhout &"
-    echo "> $galaxy$FILT_vsfh.sh"
+    echo "> $galaxy$FILT$EXT"
 done
 
 #ipcluster stop
